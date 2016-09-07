47b6477890fcc50d5b411a3b15aa0053b611ab36
sugu66 <sugunakar.bojjapally@gmail.com>
2016-09-07 15:04:49 +0530

    Add files via upload

---
+++ pages/productType.js
@@ -0,0 +1,13 @@
+module.exports = {
+  sections: {
+	selectProductType:{
+		selector: '.category-landing-page',
+			elements:{
+					tshirtsSubLink:{
+						selector : '.menu_level_2 li a[href="/shop/Men/Clothing/Activewear"]',
+											
+					}
+				}
+	}
+  }
+};
